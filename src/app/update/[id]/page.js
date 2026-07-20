dbeb8cbb891e4cfdfbe090b05594bb1275fc988b
Raspbsb <unionbjh@naver.com>
2026-07-20 16:06:26 +0900

    Implement update functionality with form for editing topics

---
+++ src/app/update/[id]/page.js
@@ -0,0 +1,93 @@
+"use client";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function Update() {
+  console.log("Update 페이지 작동");
+  const router = useRouter();
+  const { id } = useParams();
+
+  const [title, setTitle] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const options = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title,
+        message,
+      }),
+    };
+
+    fetch(`http://localhost:9999/topics/${id}`, options)
+      .then(res => res.json())
+      .then(result => {
+        router.push(`/read/${id}`);
+      });
+  };
+
+  useEffect(() => {
+    fetch(`http://localhost:9999/topics/${id}`)
+      .then(res => res.json())
+      .then(result => {
+        setTitle(result.title);
+        setMessage(result.message);
+      });
+  }, [id]);
+
+  return (
+    <>
+      <h3 style={styles.title}>Update Form</h3>
+
+      <form action="" onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            Email address
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            name="title"
+            placeholder="글의 제목을 입력해주세요"
+            value={title}
+            onChange={e => {
+              setTitle(e.target.value);
+            }}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="message" className="form-label">
+            Message
+          </label>
+          <textarea
+            name="message"
+            className="form-control"
+            id="message"
+            rows="3"
+            value={message}
+            onChange={e => {
+              setMessage(e.target.value);
+            }}
+          ></textarea>
+
+          <button type="submit" className="btn btn-primary">
+            입력
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+const styles = {
+  title: {
+    color: "green",
+  },
+};
--- src/app/update/page.js
+++
@@ -1,9 +0,0 @@
-export default function Update() {
-  console.log("Update 페이지 작동");
-
-  return (
-    <>
-      <h3>Update Form</h3>
-    </>
-  );
-}
